884db6cce3dfd0c2ca00c19a7982f878d29de766
Eldon Luk <ebl9rd@virginia.edu>
2019-09-23 20:25:12 -0400

    added header using php, needs to beautify

---
+++ header.php
@@ -0,0 +1,26 @@
+<html>
+	<body>
+		<h1><a href="index.html">TalkToMe</a></h1>
+					<nav id="nav">
+						<ul>
+							<li><a href="index.html">Home</a></li>
+							<li>
+								<a href="#" class="icon solid fa-angle-down">Browse</a>
+								<ul>
+		<li><a href="index.html">Home</a></li>
+		<li><a href="AboutUs.html">About Us</a></li>
+		<li><a href="SignUp.html">Sign Up</a></li>
+		<li><a href="Login.html">Login</a></li>
+		<li><a href="AboutUs.html">About Us</a></li>
+		<li><a href="contact.html">Contact</a></li>
+		<li><a href="SignUp.html">Sign Up</a></li>
+		<li><a href="Login.html">Login</a></li>
+		</ul>
+							</li>
+							<li><a href="#" class="button">Sign Up</a></li>
+						</ul>
+					</nav>
+	</body>
+	
+</html>
+
