97a12790c82aa0dd5f516eeba14683137e739dc3
Eldon Luk <ebl9rd@virginia.edu>
2019-09-23 20:51:40 -0400

    beautified, but doesnt show up on all pages

--- header.php
+++ header.php
@@ -1,22 +1,29 @@
 <html>
+	<head>
+		<title>Talk2Me</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
 	<body>
-		<h1><a href="index.php">TalkToMe</a></h1>
-		<nav id="nav">
-			<ul>
-				<li><a href="index.php">Home</a></li>
-				<li>
-					<a href="#" class="icon solid fa-angle-down">Browse</a>
-					<ul>
-						<li><a href="index.php">Home</a></li>
-						<li><a href="AboutUs.php">About Us</a></li>
-						<li><a href="SignUp.php">Sign Up</a></li>
-						<li><a href="Login.php">Login</a></li>
-						<!--<li><a href="contact.html">Contact</a></li>-->
-					</ul>
-				</li>
-				<li><a href="#" class="button">Sign Up</a></li>
-			</ul>
-		</nav>
+		<header id="header" class="alt">
+			<h1><a href="index.php">TalkToMe</a></h1>
+			<nav id="nav">
+				<ul>
+					<!--<li>-->
+						<!--<a href="#" class="icon solid fa-angle-down">Browse</a>
+						<ul>-->
+					<li><a href="index.php">Home</a></li>
+					<li><a href="AboutUs.php">About Us</a></li>
+					<li><a href="SignUp.php">Sign Up</a></li>
+					<li><a href="Login.php">Login</a></li>
+							<!--<li><a href="contact.html">Contact</a></li>-->
+					<!--	</ul>-->
+					<!--</li>-->
+					<li><a href="#" class="button">Sign Up</a></li>
+				</ul>
+			</nav>
+		</header>
 	</body>
 </html>
 
