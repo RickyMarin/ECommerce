441b608e5b2d6c88c0376069976092352e79b17b
vivianpham8 <52727841+vivianpham8@users.noreply.github.com>
2019-09-29 17:54:00 -0400

    added contact to header

--- header.php
+++ header.php
@@ -16,12 +16,12 @@
 						<ul>-->
 					<li><a href="index.php">Home</a></li>
 					<li><a href="AboutUs.php">About Us</a></li>
-					<li><a href="SignUp.php">Sign Up</a></li>
+					<li><a href="contact.php">Contact Us</a></li>
 					<li><a href="Login.php">Login</a></li>
 							<!--<li><a href="contact.html">Contact</a></li>-->
 					<!--	</ul>-->
 					<!--</li>-->
-					<li><a href="#" class="button">Sign Up</a></li>
+					<li><a href="SignUp.php" class="button">Sign Up</a></li>
 				</ul>
 			</nav>
 		</header>
