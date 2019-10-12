6156457800504ccd18e6f2d97d6c0d7090356efa
Eldon Luk <ebl9rd@virginia.edu>
2019-10-11 20:03:07 -0400

    added tabs to about us

--- AboutUs.php
+++ AboutUs.php
@@ -38,53 +38,86 @@
 				</header>-->
 
 			<!-- Main -->
-				<section id="main" class="container">
-					<header>
-						<h2>About Us</h2>
-						<p>What is Talk2Me?</p>
-					</header>
-					<div class="box">
-						<h3>What is Talk2Me?</h3>
-						<p> Talk2Me is a platform where fans can book a video call with their favorite celebrities.
-							Talk2Me is a fast and easy service with instaneous, high quality calls as well as guaranteed
-							appointments with our partner celebrities. </p>
-						<div class="row">
-							<div class="row-6 row-12-mobilep">
-								<h3>What is our mission?</h3>
-								<p>Our mission is to aid the creation of meaningful interactions for fans. In the day and age
-								of modern social media, with our fast-paced society, meaningful conversations are becoming more of a relic
-								of the past, lowering the amount of quality face-to-face interactions that humans were founded on as social creatures.
-								Our goal is to help revamp these in-person interactions, while at the same time serving the huge popularity out there
-								today for celebrities, from Hollywood stars to athletes to YouTube stars. </p>
-							</div>
-							<div class="row-6 row-12-mobilep">
-								<h3>How Does Talk2Me Work?</h3>
-								<p>Fans can browse through the Talk2Me list of celebrities and select the one
-									they would like to interact with over a video call. They will fill out a form
-									with their availability for the video call. After purchase and confirmation that
-									the celebrity will be able to conduct the video call, Talk2Me will generate a one-time
-									use link to a video chat platform.</p>
-							</div>
-							<div class="row-6 row-12-mobilep">
-								<h3>Which celebrities are we partnered with?</h3>
-								<p>Our celebrity list grows day by day and we do not discriminate on industry. So long as they have
-								a significant following, they can be our partners! The industries convered range from movie stars to NCAA athletes
-								to Instagram models, and much more! If your favority celebrity is not currently on our list, feel free to <a href="contact.php">Contact Us</a>
-								and suggest your celebrity!</p>
-							</div>
-							<div class="row-6 row-12-mobilep">
-								<h3>Why Should I Use Talk2Me?</h3>
-								<p>Have you ever looked at a celebrity and thought, <i>"Wow, we would probably be great friends if
-									they got to know me"</i>? Talk2Me gives you this rare opportunity! Talk2Me is a fun way
-									to get to know your favorite celebrities through an intimate conversation. Why <i>shouldn't</i>
-									you use Talk2Me?</p>
-							</div>
+			<script>
+				function openPage(pageName, elmnt, color) {
+				  // Hide all elements with class="tabcontent" by default */
+				  var i, tabcontent, tablinks;
+				  tabcontent = document.getElementsByClassName("tabcontent");
+				  for (i = 0; i < tabcontent.length; i++) {
+					tabcontent[i].style.display = "none";
+				  }
+				
+				  // Remove the background color of all tablinks/buttons
+				  tablinks = document.getElementsByClassName("tablink");
+				  for (i = 0; i < tablinks.length; i++) {
+					tablinks[i].style.backgroundColor = "";
+				  }
+				
+				  // Show the specific tab content
+				  document.getElementById(pageName).style.display = "block";
+				
+				  // Add the specific color to the button used to open the tab content
+				  //elmnt.style.backgroundColor = color;
+				}
+				
+				// Get the element with id="defaultOpen" and click on it
+				document.getElementById("defaultOpen").click();
+		</script>
+		
+		<section id="main" class="container">		
+			<h2 style="text-align:center;">About Us</h2>
+			<div class="tab">
+				<button class="tablink" onclick="openPage('What is', this, 'green')" id="defaultOpen">What is Talk2Me?</button>
+				<button class="tablink" onclick="openPage('Who', this, 'red')">Who are we?</button>
+			</div>
+			<div id="What is" class="tabcontent">
+				<div class="box">
+					<h3>What is Talk2Me?</h3>
+					<p> Talk2Me is a platform where fans can book a video call with their favorite celebrities.
+						Talk2Me is a fast and easy service with instaneous, high quality calls as well as guaranteed
+						appointments with our partner celebrities. </p>
+					<div class="row">
+						<div class="row-6 row-12-mobilep">
+							<h3>What is our mission?</h3>
+							<p>Our mission is to aid the creation of meaningful interactions for fans. In the day and age
+							of modern social media, with our fast-paced society, meaningful conversations are becoming more of a relic
+							of the past, lowering the amount of quality face-to-face interactions that humans were founded on as social creatures.
+							Our goal is to help revamp these in-person interactions, while at the same time serving the huge popularity out there
+							today for celebrities, from Hollywood stars to athletes to YouTube stars. </p>
+						</div>
+						<div class="row-6 row-12-mobilep">
+							<h3>How Does Talk2Me Work?</h3>
+							<p>Fans can browse through the Talk2Me list of celebrities and select the one
+								they would like to interact with over a video call. They will fill out a form
+								with their availability for the video call. After purchase and confirmation that
+								the celebrity will be able to conduct the video call, Talk2Me will generate a one-time
+								use link to a video chat platform.</p>
+						</div>
+						<div class="row-6 row-12-mobilep">
+							<h3>Which celebrities are we partnered with?</h3>
+							<p>Our celebrity list grows day by day and we do not discriminate on industry. So long as they have
+							a significant following, they can be our partners! The industries convered range from movie stars to NCAA athletes
+							to Instagram models, and much more! If your favority celebrity is not currently on our list, feel free to <a href="contact.php">Contact Us</a>
+							and suggest your celebrity!</p>
+						</div>
+						<div class="row-6 row-12-mobilep">
+							<h3>Why Should I Use Talk2Me?</h3>
+							<p>Have you ever looked at a celebrity and thought, <i>"Wow, we would probably be great friends if
+								they got to know me"</i>? Talk2Me gives you this rare opportunity! Talk2Me is a fun way
+								to get to know your favorite celebrities through an intimate conversation. Why <i>shouldn't</i>
+								you use Talk2Me?</p>
 						</div>
 					</div>
-					<div style="text-align: center;">
-						<a href="SignUp.php" class="button">Sign Up Now</a>
-					</div>
-				</section>
+				</div>
+				<div style="text-align: center;">
+					<a href="SignUp.php" class="button">Sign Up Now</a>
+				</div>
+			</div>
+			<div id="Who" class="tabcontent">
+				<div class="box">
+					<h3>Who are we?</h3>
+				</div>
+		</section>
 
 
 			<!-- Footer -->
