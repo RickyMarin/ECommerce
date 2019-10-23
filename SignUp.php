8a7343a810c8a24ef129b5a408fda61ed59641be
vivianpham8 <52727841+vivianpham8@users.noreply.github.com>
2019-10-22 23:26:37 -0400

    radio button

--- SignUp.php
+++ SignUp.php
@@ -21,15 +21,6 @@
         width: 100%;
         height: 2em;
     }
-
-    input[type=radio]:checked + label:after {
-        border-radius: 11px;
-        height: 2em;
-        position: absolute;
-        content: " ";
-        display: block;
-        background: #004c97;
-    }
 </style>
 
 <body class="is-preload">
