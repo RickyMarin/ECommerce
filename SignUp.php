32e4f9f1fd93ec1f8410ecdaed7038191728a0fd
Eldon Luk <ebl9rd@virginia.edu>
2019-10-23 23:01:14 -0400

    changed login script

--- SignUp.php
+++ SignUp.php
@@ -16,6 +16,14 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 
+<style>
+    input[type=radio] {
+        border: 0px;
+        width: 100%;
+        height: 2em;
+    }
+</style>
+
 <body class="is-preload">
 <div id="page-wrapper">
 
