5796fdce4790e8c417ddcbfb21a7197988241adb
Eldon Luk <ebl9rd@virginia.edu>
2019-10-23 23:57:39 -0400

    if query

--- Login.php
+++ Login.php
@@ -42,9 +42,9 @@
 			$hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 			$email = makeSafe($_POST["email"]);
 			$dbconn = pg_connect("host=ec2-174-129-218-200.compute-1.amazonaws.com port=5432 dbname=d8k5ke2dtvb9ue user=lkoloaarfawvjm password=adfffbf2c20b090912c5ffe90c7fc1e3d82b0af7dd240dc20b51dac2d7a89703");
-			$query = pg_query($dbconn, "SELECT count(*) FROM users WHERE email = '$email' AND password = '$hashedPassword'");
+			$query = pg_query($dbconn, "SELECT * FROM users WHERE email = '$email' AND password = '$hashedPassword'");
 			//echo "test";
-			if($query == 1){
+			if($query){
 				header("Location:Celebrities.List.php");
 				//return true;
 			}
