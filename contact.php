2bca298aed68083021e8b4aaff7f5ecf1f18d233
vivianpham8 <52727841+vivianpham8@users.noreply.github.com>
2019-10-23 23:06:09 -0400

    send email in contact form

--- contact.php
+++ contact.php
@@ -18,6 +18,49 @@
     <!-- Header -->
     <?php include("header.php"); ?>
 
+    <?php
+        use PHPMailer\PHPMailer\PHPMailer;
+        require 'vendor/autoload.php';
+        if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['subject']) && isset($_POST['message']){
+            $mail = new PHPMailer;
+            //Tell PHPMailer to use SMTP - requires a local mail server
+            //Faster and safer than using mail()
+            $mail->isSMTP();
+            $mail->Host = 'localhost';
+            $mail->Port = 25;
+
+            //Use a fixed address in your own domain as the from address
+            //**DO NOT** use the submitter's address here as it will be forgery
+            //and will cause your messages to fail SPF checks
+            $mail->setFrom('talk2mecontact@gmail.com', 'Talk2Me');
+            //Send the message to yourself, or whoever should receive contact for submissions
+            $mail->addAddress('talk2mecontact@gmail.com', 'Talk2Me');
+            //Put the submitter's address in a reply-to header
+            //This will fail if the address provided is invalid,
+            //in which case we should ignore the whole request
+            if ($mail->addReplyTo($_POST['email'], $_POST['name'])) {
+            $mail->Subject = $_POST['subject'];
+            //Keep it simple - don't use HTML
+            $mail->isHTML(false);
+            //Build a simple message body
+            $mail->Body = <<<EOT
+                Email: {$_POST['email']}
+                Name: {$_POST['name']}
+                Message: {$_POST['message']}
+            EOT;
+            //Send the message, check for errors
+                if (!$mail->send()) {
+                //The reason for failing to send will be in $mail->ErrorInfo
+                //but you shouldn't display errors to users - process the error, log it on your server.
+                    $msg = 'Sorry, something went wrong. Please try again later.';
+                } else {
+                    $msg = 'Message sent! Thanks for contacting us.';
+                }
+            } else {
+                $msg = 'Invalid email address, message ignored.';
+            }
+        }
+        ?>
     <!-- Main -->
     <section id="main" class="container medium">
         <header>
