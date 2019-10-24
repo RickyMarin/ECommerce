a17139f89760bd1ca65352b28ea5c7ba6d630d62
vivianpham8 <52727841+vivianpham8@users.noreply.github.com>
2019-10-23 21:04:08 -0400

    test phpmailer

--- SignUp.php
+++ SignUp.php
@@ -23,6 +23,35 @@
     <!-- Header -->
     <?php include("header.php"); ?>
 
+    <?php
+    /* Namespace alias (don't need Exception this time). */
+    use PHPMailer\PHPMailer\PHPMailer;
+
+    /* Include the Composer generated autoload.php file. */
+    require 'C:\xampp\composer\vendor\autoload.php';
+
+    /* Create a new PHPMailer object. */
+    $mail = new PHPMailer();
+
+    /* Set the mail sender. */
+    $mail->setFrom('darth@empire.com', 'Darth Vader');
+
+    /* Add a recipient. */
+    $mail->addAddress('palpatine@empire.com', 'Emperor');
+
+    /* Set the subject. */
+    $mail->Subject = 'Force';
+
+    /* Set the mail message body. */
+    $mail->Body = 'There is a great disturbance in the Force.';
+
+    /* Finally send the mail. */
+    if (!$mail->send())
+    {
+        /* PHPMailer error. */
+        echo $mail->ErrorInfo;
+    }?>
+
     <section id="main" class="container medium">
         <header>
             <h2>Sign Up</h2>
