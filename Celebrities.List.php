bfb76029f191be0e58fde0cc140dc3dbed8e0397
Eldon Luk <ebl9rd@virginia.edu>
2019-10-25 22:23:30 -0400

    finished backend code

--- Celebrities.List.php
+++ Celebrities.List.php
@@ -13,9 +13,7 @@
         
         <?php
         use PHPMailer\PHPMailer\PHPMailer;
-        if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['Password']) && isset($_POST['Address'])
-            && isset($_POST['City']) && isset($_POST['State']) && isset($_POST['ZipCode']) && isset($_POST['Age'])
-            && isset($_POST['FavoriteCelebrity']) && isset($_POST['membership'])) {
+        if(isset($_POST['email']) && isset($_POST['datetime'])) {
             require 'vendor/autoload.php';
 
             $mail = new PHPMailer(); // create a new object
@@ -29,8 +27,8 @@
             $mail->Username = "talk2mecontact@gmail.com";
             $mail->Password = "RickyEldonVivian";
             $mail->SetFrom("talk2mecontact@gmail.com");
-            $mail->Subject = "Welcome to Talk2Me!";
-            $mail->Body = "Thank you for signing up for Talk2Me! Get ready to begin talking to your favorite celebrities.";
+            $mail->Subject = "Appointment has been set!";
+            $mail->Body = "Thank you for setting up your appointment with " . $_POST['celebrity'] . " on " . $_POST['datetime'] . "! A representative will reach out soon to confirm the call.";
             $mail->AddAddress($_POST["email"]);
             $mail->Send();
             }
@@ -43,20 +41,21 @@
                      width: 300px;
                      height: 100px;
                      display: inline-block;
+                     margin-right:10px;
                     }
+                    /*#name {display:inline-block;margin-right:10px;}
+                    #email {display:inline-block;margin-right:10px;}
+                    #datetime {display:inline-block;margin-right:10px;} 
+                    #submit {display:inline-block;} */
                 </style>
                 <h2>The following celebrities can currently be scheduled:</h2>
                 <div class="us">
-                    <img src="Tinay Fey.jpeg" alt="Tina Fey" width="180" height="180" align="left">
+                    <img src="Tinay Fey.jpeg" alt="Tina Fey" width="180" height="180" align="left" margin-right:10px>
 					<h3 style="text-align:left">Tina Fey</h3>
 					<h4 style="text-align:left">Actress</h4>
 					<p style="text-align:left; color:black">Tina Fey is an American actress, comedian, writer, producer, and playwright. She is best known for her work on the NBC sketch comedy series Saturday Night Live and for creating the acclaimed comedy series 30 Rock and Unbreakable Kimmy Schmidt. </p>
                     <form id="insert" name="insert" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>"
                     method="POST">
-                        <div class="oneline">
-                            <label for="name">Name:</label>
-                            <input type="text" name="name" id="name" value="" placeholder="Name"/>
-                        </div>
                         <div class="oneline">
                             <label for="email">Email:</label>
                             <input type="email" name="email" id="email" value="" placeholder="Email"/>
@@ -65,7 +64,8 @@
                             <label for="datetime">Date and time desired for call:</label>
                             <input type="datetime-local" name="datetime" id="datetime" value=""/>
                         </div>
-                        <input type="submit" value="Schedule now!"/>
+                        <input type="submit" value="Schedule now!" id="submit"/>
+                        <input type="hidden" id="celebrity" value="Tina Fey"/>
                     </form>
                 </div>
             </div>
