3877bac32383751630498d2726f54570ce29aa4a
Ricardo Marin <ram8ny@virginia.edu>
2019-11-10 15:29:08 -0500

    fuurther testing

--- LoggedInHeader.php
+++ LoggedInHeader.php
@@ -10,9 +10,7 @@
 		<header id="header">
             <a class="image-link"><img src="images/logo.png" width="100" height="100"></a>
 			<nav id="nav">
-            <form action="logout.php">
-                    <input type="button" id="logout" value="Log out" name="logout">
-                </form>
+                <a href='logout.php'>Click here to log out</a>
 			</nav>
 		</header>
 	</body>
